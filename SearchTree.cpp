e0afec7ce2342d18fa4824a097280bcf3daece0b
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-11 11:34:31 +0700

    menabahkan menu pilihan operasi pohon di fungsi main

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -125,3 +125,25 @@ public:
         }
     }
 };
+
+int main()
+{
+    BinaryTree obj;
+
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. implementasi insert operation" << endl;
+        cout << "2. perform inorder traversal" << endl;
+        cout << "3. perform preorder traversal" << endl;
+        cout << "4, perform postorder traversal" << endl;
+        cout << "5. exit" << endl;
+        cout << "\nEnter your choice (1-5) ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+        
+    }
+}

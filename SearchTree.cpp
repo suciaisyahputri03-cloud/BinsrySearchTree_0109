2a3c884e1d09c73d2c2a773c12cc31b399a91dcc
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-11 09:31:38 +0700

    menambahkan conscructor unutuk kelas node

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -8,5 +8,11 @@ public:
     Node *leftchild;
     Node *rightchild;
 
-    
+    // constructor for the node class 
+    Node(string i, Node *1, Node *r)
+    {
+        info = i;
+        leftchild = 1;
+        rightchild = r;
+    }
 };

0e426eb6180e0b4f1037f6ba75993ff5287a8788
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-11 11:27:33 +0700

    menamabhkan fungsi postorder  untuk mencetak data

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -106,4 +106,22 @@ public:
             preorder(ptr->rightchild);
         }
     }
+    
+    void postorder(Node *ptr)
+    {
+        // performs the postorder traversal of the utree
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
